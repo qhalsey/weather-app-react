bcb2c44f5589aa15a7e15bfb30fe9a3c267eb2d8
quentin halsey <qhalseyhb@yahoo.com>
2018-12-05 19:29:45 -0800

    create react app template with 3 components so far, starting to use API for data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+class App extends React.Component {
+
+  getWeather = () => {
+    const api_call = await fetch('api.openweathermap.org/data/2.5/weather?q=HuntingtonBeach,us&appid=${API_KEY}&units=metric');
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render(){
+    return(
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
